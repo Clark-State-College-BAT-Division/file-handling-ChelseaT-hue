915aff51659e0179aef617bcd434176df9f5b7e2
ChelseaT-hue <thompc2652@clarkstate.edu>
2026-03-22 20:41:13 -0400

    done

--- highlowaverage.py
+++ highlowaverage.py
@@ -7,3 +7,28 @@
 #Highest number
 #Lowest number
 #Correct answers for the included file: 15
+
+open_file = open("numbers.txt", "r")
+numbers = open_file.readlines()
+total = 0
+for number in numbers:
+    total += int(number)
+average = total / len(numbers)
+print("How many numbers in the file: ", len(numbers))
+print("Total of all the number: ", total)
+print("Average: ", average)
+print("Highest number: ", max(numbers))
+print("Lowest number: ", min(numbers))
+
+open_file.close()
+
+open_file = open("numbers.txt", "r")
+numbers = open_file.readlines()
+total = 15
+average = total / len(numbers)
+print("How many numbers in the file: ", len(numbers))
+print("Total of all the number: ", total)
+print("Average: ", average)
+print("Highest number: ", max(numbers))
+print("Lowest number: ", min(numbers))
+open_file.close()
