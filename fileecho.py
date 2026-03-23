04a33f51390c8384c4c726d1a1a8b55e4c993223
ChelseaT-hue <thompc2652@clarkstate.edu>
2026-03-22 20:51:29 -0400

    done

--- fileecho.py
+++ fileecho.py
@@ -1,2 +1,12 @@
 #Tead in the echo.txt file and display it in the terminal using print()
 #This is a guided practice. Either follow the video or your instructor in class.
+
+open_file = open("echo.txt", "r")
+echo = open_file.read()
+print(echo)
+open_file.close()
+
+open_file = open('echo.txt', 'r')
+echo = open_file.read()
+print(firstLine)
+open_file.close()
