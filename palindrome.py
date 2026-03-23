3ffa1d629a099ec7a60044cff6cedf35dbae9c2b
ChelseaT-hue <thompc2652@clarkstate.edu>
2026-03-22 20:44:43 -0400

    done

--- palindrome.py
+++ palindrome.py
@@ -3,3 +3,14 @@
 #For example, wow is a paldindrome.
 #A different file wile be used for grading.
 #Correct answer for this file: 
+
+open_file = open("words.txt", "r")
+words = open_file.readlines()
+palindrome_count = 0
+for word in words:
+    word = word.strip()
+    if word == word[::-1]:
+        palindrome_count += 1
+print("Number of palindromes: ", palindrome_count)
+open_file.close()
+
